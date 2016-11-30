48c46397a1432f4270753b61e3c3b18df6c4369d
nino1201 <a.nino1201@gmail.com>
2016-11-30 11:26:44 -0500

    estoy a punto

--- punto1/prob.py
+++ punto1/prob.py
@@ -5,4 +5,4 @@ files=np.genfromtxt('sismos.dat')
 
 figure = corner.corner(files,labels=[r"$x$", r"$y$", r"$\Gamma \, [\mathrm{parsec}]$"],quantiles=[0.16, 0.5], show_titles=True ,title_kwargs={"fontsize": 12})
 
-figure.savefig("sismos.png")
+figure.savefig("sismos.pdf")
--- punto2/prob.py
+++ punto2/prob.py
@@ -5,4 +5,4 @@ files=np.genfromtxt('planetas.dat')
 
 figure = corner.corner(files,labels=[r"$n$", r"$log(M)$", r"$\Gamma \, [\mathrm{parsec}]$"],quantiles=[0.16, 0.5],show_titles=True, title_kwargs={"fontsize": 12})
 
-figure.savefig("planetas.png")
+figure.savefig("planetas.pdf")
