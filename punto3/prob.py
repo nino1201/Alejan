5a4c5b3d4896ba8d3f94a298ed80dc17dceba9a0
nino1201 <a.nino1201@gmail.com>
2016-11-30 08:09:26 -0500

    no jodas

--- punto3/prob.py
+++ punto3/prob.py
@@ -3,6 +3,6 @@ import numpy as np
 
 files=np.genfromtxt('biologos.dat')
 
-figure = corner.corner(files, title_kwargs={"fontsize": 12})
+figure = corner.corner(files,labels=[r"$\alpha$", r"$\beta$",r"$\gamma$",r"$\delta$", r"$\Gamma \, [\mathrm{parsec}]$"],quantiles=[0.16, 0.5,0.16,0.15], show_titles=True, title_kwargs={"fontsize": 12})
 
 figure.savefig("biologos.png")
