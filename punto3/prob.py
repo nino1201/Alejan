afbfc08437979b400eefd56134aa272ee9b599cd
nino1201 <a.nino1201@gmail.com>
2016-11-30 11:30:47 -0500

    epa epa colombia

--- punto3/prob.py
+++ punto3/prob.py
@@ -5,4 +5,4 @@ files=np.genfromtxt('biologos.dat')
 
 figure = corner.corner(files,labels=[r"$\alpha$", r"$\beta$",r"$\gamma$",r"$\delta$", r"$\Gamma \, [\mathrm{parsec}]$"],quantiles=[0.16, 0.5,0.16,0.15], show_titles=True, title_kwargs={"fontsize": 12})
 
-figure.savefig("biologos.png")
+figure.savefig("biologos.pdf")
